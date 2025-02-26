da6294bff53dbedce661c87bb63be74f7d5357b4
Philmore Koung <126188634+philmorefkoung@users.noreply.github.com>
2025-02-25 18:19:09 -0600

    Update CNN-binaryclass.py

--- models/binaryclass/CNN-binaryclass.py
+++ models/binaryclass/CNN-binaryclass.py
@@ -21,7 +21,7 @@ test_dataset = test_dataset.batch(batch_size)
 base_model = tf.keras.applications.ResNet50( # Swap between DenseNet121 and Xception
     weights= 'imagenet',  # Pre-trained for full datasets, 'None' for limited data setting
     include_top=False,  
-    input_shape=(128, 128, 3) # 128x128 for all datasets except ALL-IDB2 which is preprocessed to 224x224
+    input_shape=(128, 128, 3) # 128x128 for all datasets except ALL-IDB2 and AML which is preprocessed to 224x224
 )
 
 # Freeze base model
